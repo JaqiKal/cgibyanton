7684fdbbad90b51af619e0a74ef9e8d47039e90f
JaqiKal <jaqika@gmail.com>
2024-12-21 18:43:24 +0100

    fix: Direct the root (/) to the Hero component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Hero from "./components/Hero";  // Ensure the correct import
 
 const App = () => {
   return (
     <Router basename="/cgibyanton">
       <Routes>
-        <Route path="/home" element={<Home />} />
-        {/* Other routes */}
+        {/* Direct the root (/) to the Hero component */}
+        <Route path="/" element={<Hero />} />
+        {/* Add other routes as needed */}
       </Routes>
     </Router>
   );
